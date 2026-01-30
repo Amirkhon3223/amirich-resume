7b9110a4d8ebd69e0adee186b78af15c93b75e06
Amirkhon3223 <amirich3223@outlook.com>
2026-01-30 14:18:34 -0500

    amirich update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { WorksComponent } from './pages/works/works.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'projects', component: WorksComponent },
 ];
